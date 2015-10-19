27d286d82d5298e55551bde0d5fbdae7099fc24f
jamierose <jrose111284@gmail.com>
2015-10-19 16:34:38 +0100

    chapter 4

--- lib/oystercards.rb
+++ lib/oystercards.rb
@@ -1,3 +1,8 @@
 class OysterCards
-  
+  DEAFUALT_BALANCE = 0
+  attr_reader :money
+  def initialize(money=DEAFUALT_BALANCE)
+    @money = 0
+  end
+
 end
--- spec/oystercards_spec.rb
+++ spec/oystercards_spec.rb
@@ -1,5 +1,9 @@
 require 'oystercards'
 
 describe OysterCards do
-
+  describe '#initialize_balance' do
+  it 'has a default of 0' do
+  expect(subject.money).to eq 0
+  end
+ end
 end
