2d0b701821101f5a2039d0019f061f0a5a772a5c
Tom Barnes <ruralmyth@gmail.com>
2015-10-21 12:40:58 +0100

    ch.13 added station class and first tests

---
+++ spec/station_spec.rb
@@ -0,0 +1,18 @@
+require 'station'
+
+describe Station do 
+  
+  subject(:station) { described_class.new(:name, :zone) }
+
+  describe 'initialization' do
+    it 'a new station has a name' do
+      expect(station.name).to eq name
+    end
+
+    it 'a new station has a zone' do
+      expect(station.zone).to eq zone
+    end
+  end
+  
+end
+
